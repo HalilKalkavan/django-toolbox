5c252ec750fd3228393821ac2c096835b8a3225f
HalilKalkavan <halilkalkavan@gmail.com>
2021-08-21 04:10:29 -0400

    Delet existing project and create new fresh one

--- firstProject/myApp/apps.py
+++ firstProject/myApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SecondappConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'secondApp'
+    name = 'myApp'
