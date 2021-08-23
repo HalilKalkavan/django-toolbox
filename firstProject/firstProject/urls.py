af3e02efb97cf91c88c906590b88caabd2fe16cb
HalilKalkavan <halilkalkavan@gmail.com>
2021-08-23 00:29:42 -0400

    Working with urls.py

--- firstProject/firstProject/urls.py
+++ firstProject/firstProject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myApp import views
 
 urlpatterns = [
+    path('', views.index),
     path('myApp/', include('myApp.urls')),
     path('admin/', admin.site.urls),
 ]
